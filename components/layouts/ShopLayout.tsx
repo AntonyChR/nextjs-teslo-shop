67ca982103359844ca00a3ef37d99c35e0677910
AntonyChR <antonychr99@gmail.com>
2022-03-29 18:06:29 -0500

    ADD: Layout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import { FC } from 'react';
+import { Navbar } from '../ui';
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageUrl?: string;
+}
+
+export const ShopLayout: FC<Props> = ({ children, title, pageDescription, imageUrl }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={pageDescription} />
+        {imageUrl && <meta property="og:image" content={imageUrl} />}
+      </Head>
+      <nav>
+        <Navbar/>
+      </nav>
+      <main
+        style={{
+          margin: '80px auto',
+          maxWidth: '1440px',
+          padding: '0px 30px',
+        }}
+      >
+        {children}
+      </main>
+      <footer></footer>
+    </>
+  );
+};
