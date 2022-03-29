b65e9c6f234b6a2b50911f2e0877c31f016e8d77
AntonyChR <antonychr99@gmail.com>
2022-03-29 18:09:39 -0500

    ADD: Custom page for 404 error

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
+import { ShopLayout } from '../components/layouts';
+
+const PageNotFound = () => {
+    return (
+        <ShopLayout title="Page not found" pageDescription="No hay nada que mostrar">
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                height="calc(100vh - 200px)"
+            >
+                <img height="200px" src="/error-404.png" alt="404" />
+                <Typography variant="h1">Esta página no existe</Typography>
+            </Box>
+        </ShopLayout>
+    );
+};
+
+export default PageNotFound;
