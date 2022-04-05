f9e707968bcb3f71df5bfe25d7c8d56894136226
AntonyChR <antonychr99@gmail.com>
2022-04-05 18:26:49 -0500

    ADD: SSR for product page

--- database/index.ts
+++ database/index.ts
@@ -1,4 +1,7 @@
 
 export * as db from './db';
-export * as seedDatabase from './products'
+export * as dbProducts from './dbProducts';
 export * from './constants'
+
+//test db
+export * as seedDatabase from './products'
--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,18 +1,19 @@
-import { Box, Button, Chip, Grid, Typography } from '@mui/material';
+import { NextPage } from 'next';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../../components/layouts';
 import { ProductSlideShow, SizeSelector } from '../../components/products';
 import { ItemCounter } from '../../components/ui';
-import { initialData } from '../../database/products';
+import { IProduct } from '../../interfaces';
 
-const product = initialData.products[0];
-
-const ProductPage = () => {
-    
+interface Props {
+    product: IProduct;
+}
+const ProductPage: NextPage<Props> = ({ product }) => {
     return (
         <ShopLayout title={product.title} pageDescription={product.description}>
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={7}>
-                    <ProductSlideShow images={product.images}/>
+                    <ProductSlideShow images={product.images} />
                 </Grid>
                 <Grid item xs={12} sm={5}>
                     <Box display="flex" flexDirection="column">
@@ -24,8 +25,8 @@ const ProductPage = () => {
                         </Typography>
                         <Box sx={{ my: 2 }}>
                             <Typography variant="subtitle2">Cantidad</Typography>
-                            <ItemCounter/>
-                            <SizeSelector selectedSize={product.sizes[1]} sizes={product.sizes}/>
+                            <ItemCounter />
+                            <SizeSelector selectedSize={product.sizes[1]} sizes={product.sizes} />
                         </Box>
                         <Button color="secondary" className="circular-btn">
                             Agregar al carrito
@@ -40,6 +41,30 @@ const ProductPage = () => {
             </Grid>
         </ShopLayout>
     );
+};
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+import { GetServerSideProps } from 'next';
+import { dbProducts } from '../../database';
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const { slug } = params as { slug: string }; // your fetch function here
+
+    const product = await dbProducts.getProductBySlug(slug);
+
+    if (!product) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {product},
+    };
 };
 
 export default ProductPage;
